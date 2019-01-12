19be94c3e6c951d9c6627ab2a7eea40535aae30d
zintrulcre <zintrulcre@gmail.com>
2019-01-12 16:29:32 +1100

    read files from folder

--- ECNU_Library_Reservation.py
+++ ECNU_Library_Reservation.py
@@ -1,8 +1,10 @@
+import os
 import json
 import requests
 
 
 class LibraryReserve:
+    files = []
     urls = ["http://202.120.82.2:8081/ClientWeb/pro/ajax/login.aspx",
             "http://202.120.82.2:8081/ClientWeb/pro/ajax/reserve.aspx"]
     headers = {
@@ -12,9 +14,25 @@ class LibraryReserve:
                  "type": "dev", "act": "set_resv",
                  "_": "1547215554524"}
     result_info = []
+    num = 0
 
-    def ConcatInfo(self):
-        file = open('./config.json', 'r')
+    def __init__(self):
+        pass
+
+    def Run(self):
+        files = os.listdir("./reservations")
+        for file in files:
+            if not os.path.isdir(file):
+                name = str(file)
+                if(name == ".DS_Store"):
+                    continue
+                num = name[name.find('_') + 1:name.find('.')]
+                self.ConcatInfo("./reservations/" + file)
+                self.Reserve()
+                self.Result(num)
+
+    def ConcatInfo(self, config_file):
+        file = open(config_file, 'r')
         info = json.load(file)
         self.user_info['id'] = info['student ID']
         self.user_info['pwd'] = info['password']
@@ -38,7 +56,7 @@ class LibraryReserve:
             self.result_info.append(key+' = ' + value+'\n')
         self.result_info.append('\n')
 
-    def run(self):
+    def Reserve(self):
         login_post = requests.post(url=self.urls[0],
                                    data=self.user_info,
                                    headers=self.headers)
@@ -58,12 +76,18 @@ class LibraryReserve:
         output = text[text.find('msg":"') + 6:text.find('","data"')]
         print(output)
 
-    def PrintResult(self):
-        file = open('./result.md', 'w')
+    def Result(self, num):
+        file_name = "./results/"+"result_" + str(num) + ".md"
+        file = open(file_name, 'w')
         file.writelines(self.result_info)
+        del self.user_info['id']
+        del self.user_info['pwd']
+        del self.time_info['start_time']
+        del self.time_info['end_time']
+        del self.time_info['start']
+        del self.time_info['end']
+        self.result_info.clear()
 
 
 libraryReserve = LibraryReserve()
-libraryReserve.ConcatInfo()
-libraryReserve.run()
-libraryReserve.PrintResult()
+libraryReserve.Run()
